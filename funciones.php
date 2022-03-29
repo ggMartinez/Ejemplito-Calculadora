f24e58209910e17becccce1f25b39622e6a05161
Gonzalo Martinez <gon.martinez@icloud.com>
2022-03-28 23:00:03 -0300

    Primer commit

---
+++ funciones.php
@@ -0,0 +1,25 @@
+<?php 
+
+    function sumar($sumando1,$sumando2){
+        echo "$sumando1 + $sumando2 =";
+        echo $sumando1 + $sumando2;
+    }
+    function restar($sustraendo,$minuendo){
+        echo "$sustraendo - $minuendo =";
+        echo $sustraendo - $minuendo;
+    }
+
+    function multiplicar($factor1,$factor2){
+        echo "$factor1 x $factor2 =";
+        echo $factor1 * $factor2;
+    }
+
+    function dividir($dividendo,$divisor){
+        echo "$dividendo / $divisor =";
+        echo $dividendo / $divisor;
+    }
+
+    function elevarAlCuadrado($base){
+        echo "$base ^ 2 = ";
+        echo $base * $base;
+    }
---
+++ numeros.php
@@ -0,0 +1,16 @@
+<?php 
+
+    require "funciones.php";
+
+
+    $numero1 = $_GET['numero1'];
+    $numero2 = $_GET['numero2'];
+    $operacion = $_GET['operacion'];
+  
+    if($operacion === "sum") sumar($numero1,$numero2);
+    if($operacion === "res") restar($numero1,$numero2);
+    if($operacion === "mul") multiplicar($numero1,$numero2);
+    if($operacion === "div") dividir($numero1,$numero2);
+    if($operacion === "cuad") elevarAlCuadrado($numero1);
+
+
