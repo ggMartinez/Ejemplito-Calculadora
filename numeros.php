d65ab0f36035f2f9b392e6d18cf3138671099078
Gonzalo Martinez <gon.martinez@icloud.com>
2022-04-22 21:43:28 -0300

    Saque una linea

--- numeros.php
+++ numeros.php
@@ -7,7 +7,6 @@
     $numero2 = $_GET['numero2'];
     $operacion = $_GET['operacion'];
   
-    echo "Resultado: <br >";
     if($operacion === "sum") sumar($numero1,$numero2);
     if($operacion === "res") restar($numero1,$numero2);
     if($operacion === "mul") multiplicar($numero1,$numero2);
