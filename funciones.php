ec4cb599fe9030a3db1e27ded065b9b3e86e74ca
Gonzalo 2 <ggmartinez.utu@gmail.com>
2022-04-26 01:26:47 +0000

    Modificadas funciones y agregada raiz cuadrada

--- funciones.php
+++ funciones.php
@@ -1,12 +1,16 @@
 <?php 
 
     function sumar($sumando1,$sumando2){
-        echo $sumando1 + $sumando
+        echo $sumando1 + $sumando2;
     }
 
     function multiplicar($factor1,$factor2){
-        echo "$factor1 x $factor2 =";
+        echo "$factor1 multiplicado por $factor2 es igual a ";
         echo $factor1 * $factor2;
+    }
+
+    function raizCuadrada($numero){
+	echo sqrrt($numero);
     }
 
 
